cf2764e96126941c60f585cfa1e70fa51471e2bc
timyilmaz559-tech <timyilmaz559@gmail.com>
2026-01-01 16:03:31 +0300

    Update picam.py

--- picam.py
+++ picam.py
@@ -1,36 +1,22 @@
-# kamera_only.py
 from flask import Flask, Response
 from picamera2 import Picamera2
-import time
+import cv2
 
 app = Flask(__name__)
-
-# Kamera
 picam2 = Picamera2()
-config = picam2.create_preview_configuration(main={"size": (640, 480)})
-picam2.configure(config)
+picam2.configure(picam2.create_preview_configuration())
 picam2.start()
 
-time.sleep(2)  # Kamera ısınması
-
-def generate():
+def gen():
     while True:
         frame = picam2.capture_array()
-        
-        # JPEG'e çevir (basit)
-        import cv2
-        ret, jpeg = cv2.imencode('.jpg', frame)
-        
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + 
-               jpeg.tobytes() + b'\r\n')
-        
-        time.sleep(0.033)
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        _, jpeg = cv2.imencode('.jpg', frame)
+        yield b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n'
 
 @app.route('/')
-def index():
-    return Response(generate(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+def video():
+    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
